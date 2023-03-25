e4ba73c78fa0652cf0c05dc9f81de422810f60ad
rvanwijk <rickvanwijk@bluebirdday.nl>
2023-03-25 17:02:36 +0100

    added router base path

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   target: 'static',
   router: {
-    base: '/<repository-name>/'
+    base: '/interactive-card/'
   },
   ssr: false,
 
