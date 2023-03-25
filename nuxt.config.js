fa3873518a3732df9e0a7431ceb3a502385c93aa
rvanwijk <rickvanwijk@bluebirdday.nl>
2023-03-25 16:55:31 +0100

    fixing issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  target: 'static',
+  router: {
+    base: '/<repository-name>/'
+  },
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
